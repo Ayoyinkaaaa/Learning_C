206e43a62516145710a5f6223599b75f3f3b58fe
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-04-20 12:52:58 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - main - 5-printf.c
- * description - Write a C program that prints exactly with proper grammar,
- *
- * Return: Always 0 (success)
+ * main - Entry point
+ * Return: always 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
