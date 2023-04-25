3feb718e6f7cb5e6d9b1f71e74daa563928a7db5
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-04-25 09:33:53 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * _islower - check if char is lowercase
@@ -8,9 +8,8 @@ include "main.h"
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	}
+	else
 		return (0);
 }
