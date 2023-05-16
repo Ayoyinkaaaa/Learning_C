1bad74fda4d91e602586704a31367983b66fbee8
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-05-16 11:24:05 -0700

    alloc_grid

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,27 +16,46 @@ char *str_concat(char *s1, char *s2)
 
 		s1 = "";
 
-	if (s2 == NULL)
+	if (s2 == NULL)	
 		s2 = "";
+
 		i = ci = 0;
+
 	while (s1[i] != '\0')
-	i++;
+
+		i++;
+
 	while (s2[ci] != '\0')
-	ci++;
+
+		ci++;
+
 	conct = malloc(sizeof(char) * (i + ci + 1));
+
 	if (conct == NULL)
-		return (NULL)
+
+		return (NULL);
+
 	i = ci = 0;
+
 	while (s1[i] != '\0')
+
 	{
+
 	conct[i] = s1[i];
+
 	i++;
+
 	}
+
 	while (s2[ci] != '\0')
+
 	{
 	conct[i] = s2[ci];
+
 	i++, ci++;
+
 	}
+
 	conct[i] = '\0';
 
 	return (conct);
