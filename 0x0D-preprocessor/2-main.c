d049a7b186cdc221438278317cd748d28e82880a
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-05-22 05:16:16 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main: prints the name of the file
+ * main- prints the name of the file the program was compiled from.
+ * Description: prints all argument received
  * Return: Always 0(success)
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	
-	return(0);
+	return (0);
 }
 
