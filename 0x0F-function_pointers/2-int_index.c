eac678818cc2db741876efc147106c71f8e1b30b
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-05-24 13:13:43 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * int_index- returns the index of the first element
+ * int_index - returns the index of the first element
  * @size: is the number of elements in the array
  * @cmp: is a pointer to the function to be used to compare values
  * @array: array
@@ -11,16 +11,15 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	
+
 	if (array == NULL || cmp == NULL)
 	return (-1);
 	if (size <= 0)
 		return (-1);
-
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) !=0)
-			return (i);
+	if (cmp(array[i]) != 0)
+		return (i);
 	}
 	return (-1);
 }
