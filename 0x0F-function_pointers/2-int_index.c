0f8ae675538ee79aeb2cef5f44c77e77e6d1d649
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-05-24 12:02:45 -0700

    0-print_name.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * int_index- returns the index of the first element
@@ -10,14 +11,16 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	if (array == NULL || size <= 0 || cmp == NULL)
-		return(-1);
+	
+	if (array == NULL || cmp == NULL)
+	return (-1);
+	if (size <= 0)
+		return (-1);
 
-	for (i = 0; i < size; i++) 
+	for (i = 0; i < size; i++)
 	{
-						
-        if (cmp(array[i]))
-            return(i);
-    }
-	return(-1);
+		if (cmp(array[i]) !=0)
+			return (i);
+	}
+	return (-1);
 }
