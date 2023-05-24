e8a83356f2c89708e7a5f7456089f11584a3db7d
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-05-24 05:26:58 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-
-void print_name(char *name, void (*f)(char *)) {
-    f(name);
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints a name.
+ * @name: name of the person.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    printf("%s\n", name);
 }
 
-void print_to_console(char *name) {
-    printf("Name: %s\n", name);
-}
+int main(void)
+{
+    char name[] = "Ayoyinka";
+    print_name(name, NULL);
 
-int main() {
-    char name[] = "John Doe";
-    print_name(name, print_to_console);
     return 0;
 }
