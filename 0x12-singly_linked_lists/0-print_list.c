67ccbf05e205be2f4ddafc8e5c069688133a46a7
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-06-01 05:49:13 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t t = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d]%s\n", h->len, h->str);
-		count++;
+			printf("[%u]%s\n", h->len, h->str);
+		t++;
 		h = h->next;
 	}
-	return (count);
+	return (t);
 }
