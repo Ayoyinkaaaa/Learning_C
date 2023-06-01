0dd14123c00fd45be46296f4dc32a93ae1c5ea5c
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-06-01 06:04:02 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
 
+/**
+ * list_len - prints all the elements in the list_t list
+ *
+ * @h: a pointer to the hread of nodes in the list_t list
+ *
+ * Return: number of nodes
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
