5589b9b8cffdd3d61fd3572c2ad4ed8e7e5970ec
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-06-01 13:32:58 -0700

    pus

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void before_main(void)__attribute__((constructor));
+void first(void)__attribute__((constructor));
 
 /**
- * before_main - function that runs before main
+ * first - function that runs before main
  */
-void before_main(void)
+void first(void)
 {
 	printf("you are beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
