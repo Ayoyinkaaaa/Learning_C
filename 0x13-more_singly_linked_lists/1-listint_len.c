e3fbcb38ee4dc52df9fbba5ecd78f4382c125065
Egunjobi Ayoyinka <Egunjobikao@gmail.com>
2023-06-06 07:23:15 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,21 +2,22 @@
 #include "lists.h"
 
 /**
- * lisint_len - prints all the elements in the listint_t list.
+ * listint_len - prints all the elements in the listint_t list.
  *
  * @h: a pinter to the head nodes.
+ *
+ * description: print all arguments received.
  *
  * Return: number of nodes.
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t i;
 	const listint_t *temp = h;
 
-	while (temp != NULL)
+	for (i = 0; temp; i++)
 	{
-		count++;
 		temp = temp->next;
 	}
-	return (count);
+	return (n);
 }
