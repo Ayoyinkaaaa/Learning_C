dea662e124fb3d2011c9655155de6ad7e52ed48a
Ayoyinkaaaa <Egunjobikao@gmail.com>
2023-08-24 05:46:16 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,10 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[0] (nil)\n");
 		else
-		printf("[0] (nil)\n");
+			printf("[%d] %s\n", h->len, h->str);
+
 	count++;
 	h = h->next;
 	}
