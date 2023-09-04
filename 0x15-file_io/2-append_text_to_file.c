3b59d45464e9a52cf11187754f743aafd9dc3ec3
Ayoyinkaaaa <Egunjobikao@gmail.com>
2023-09-04 13:34:14 -0700

    changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,25 +9,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t fwrite;
+	int fd, i, fwrite;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-	{
-		return (1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+			;
 
-	fwrite = write(fd, text_content, strlen(text_content));
-	close(fd);
+	fwrite = write(fd, text_content, i);
 	if (fwrite == -1)
 		return (-1);
 	}
-
+	close(fd);
 	return (1);
 }
 
